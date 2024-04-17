41289520870c3db2b82f1e31be2f067b25f604c6
Alfredocgn <alfredocgn@gmail.com>
2024-04-16 22:38:05 -0300

    bug in get youtube video

--- Backend/ai_blog_app/blog_generator/views.py
+++ Backend/ai_blog_app/blog_generator/views.py
@@ -6,6 +6,10 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 import json
 from pytube import YouTube
+from django.conf import settings
+import os
+import assemblyai as aai
+import openai
 
 
 # Create your views here.
@@ -18,21 +22,66 @@ def generate_blog(request):
   if request.method == "POST":
     try:
       data = json.loads(request.body)
-      yt_link = data['link']
-      return JsonResponse({'content':yt_link})
+      yt_link = data['link']     
     except(KeyError,json.JSONDecodeError):
       return JsonResponse({'error':'Invalid data sent'},status=400)
     
+    title = yt_title(yt_link)
+    print(title)
 
+    transcription = get_transcription(yt_link)
+    if not transcription:
+      return JsonResponse({'error':"Failed to get transcript"},status=500)
+    
+
+    blog_content = generate_blog_from_transcription(transcription)
+    if not blog_content:
+      return JsonResponse({'error':'Failed to generate blog article'},status=500)
+    
+
+
+
+    return JsonResponse({'Content':blog_content})
     
   else:
     return JsonResponse({'error':'Invalid request method'},status=405)
 
 
 def yt_title(link):
-  yt = Youtube(link)
+  yt = YouTube(link)
   title = yt.title
   return title
+
+def download_audio(link):
+  yt = YouTube(link)
+  video = yt.streams.filter(only_audio=True).first()
+  out_file = video.download(output_path=settings.MEDIA_ROOT)
+  base, ext = os.path.splitext(out_file)
+  new_file = base + '.mp3'
+  os.rename(out_file,new_file)
+  return new_file
+
+def get_transcription(link):
+  audio_file = download_audio(link)
+  aai.settings.api_key = "1b547432f35743a9aed77a49d66c4417"
+
+  transcriber = aai.Transcriber()
+  transcript = transcriber.transcribe(audio_file)
+  return transcript.text
+
+def generate_blog_from_transcription(transcription):
+  openai.api_key= "sk-proj-URzEvihQgEYwKvPnurBTT3BlbkFJRCM0mXQU79XwCyaJ8I5N"
+  prompt = f"Based on the following transcript from a YouTube video, write a comprehensive blog article, write it based on the transcript, but dont make it look like a youtube video, make it look like a proper blog article:\n\n{transcription}\n\nArticle:"
+
+  response = openai.Completion.create(
+    model = "text-davinci-003",
+    prompt = prompt,
+    max_tokens = 1000
+  )
+
+  generated_content = response.choices[0].text.strip()
+  return generated_content
+
 
 
 def user_login(request):
